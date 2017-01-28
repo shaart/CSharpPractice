edca6e70a8640a2086b13805db7c5d500e8ba77a
Artur <shalaev-artur@yandex.ru>
2017-01-28 14:20:56 +0300

    Console command
    +Getting command from input
    +Test command 'hello'

--- ISaPI/ISaPI/Code/Console.cs
+++ ISaPI/ISaPI/Code/Console.cs
@@ -11,7 +11,20 @@ namespace ISaPI.Code
     {
         public static void ProcessInput(ref TextBox console, string input)
         {
-            console.Text += input + '\n' + '>';
+            console.Text += '>' + input + '\n';
+
+            int firstSpace = input.IndexOf(' ');
+            string command = input.Trim().Substring(0, (firstSpace > 0 ? firstSpace : input.Length) ).ToLower();
+
+            switch (command)
+            {
+                case "hello":
+                    console.Text += "Hi, user!" + '\n';
+                    break;
+                default:
+                    console.Text += "Received command: " + command + '\n';
+                    break;
+            }
         }
     }
 }
