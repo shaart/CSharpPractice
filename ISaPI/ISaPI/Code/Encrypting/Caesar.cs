a01bee963d2408100ef2331a74ebdb4c669897d4
Artur <shalaev-artur@yandex.ru>
2017-01-31 15:48:01 +0300

    TextBoxStreamWriter, console rework, rename
    == Console rework & TextBoxStreamWriter
    +System.Console stream now out into TextBox (_Console) using
    TextBoxStreamWriter
    +Added command dictionary
    +Added splitting input into tokens
    -Deleted/rewrited old console methods
    
    == Rename
    ~Renamed MainWindow -> ConsoleWindow
    +Added title to ConsoleWindow
    
    == Encrypting
    +Added initial Caesar class

---
+++ ISaPI/ISaPI/Code/Encrypting/Caesar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISaPI.Code.Encrypting
+{
+    public static class Caesar
+    {
+        public static string CommandInfo = "Command usage: caesar [input_file] [output_file]";
+
+        public static void Execute(IEnumerable<string> args)
+        {
+            Console.Write("Caesar with args: ");
+            foreach (var arg in args)
+            {
+                Console.Write(arg + ' ');
+            }
+            Console.Write('\n');
+        }
+    }
+}
---
+++ ISaPI/ISaPI/ConsoleWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+
+namespace ISaPI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class ConsoleWindow : Window
+    {
+        // Это наш специальный класс TextWriter
+        TextWriter _writer = null;
+        
+        public ConsoleWindow()
+        {
+            InitializeComponent();
+
+            _writer = new Code.TextBoxStreamWriter(_Console);
+            System.Console.SetOut(_writer);
+        }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            Code.MyConsole.ProcessInput(_Input.Text);
+            _Input.Text = "";
+        }
+    }
+}
