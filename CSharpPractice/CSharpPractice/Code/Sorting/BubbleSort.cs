b10f48bb49ef7d57182c7608cafb000fa2b71d97
Artur <shalaev-artur@yandex.ru>
2017-03-11 19:00:10 +0300

    BubbleSort refactoring

--- CSharpPractice/CSharpPractice/Code/Sorting/BubbleSort.cs
+++ CSharpPractice/CSharpPractice/Code/Sorting/BubbleSort.cs
@@ -6,26 +6,42 @@ namespace CSharpPractice.Code.Sorting
     {
         public static void Sort<T>(ref T[] a, SortingOrder order) where T : IComparable
         {
+            switch (order)
+            {
+                case SortingOrder.Ascending:
+                    SortAscending(ref a);
+                    break;
+                case SortingOrder.Descending:
+                    SortDescending(ref a);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown sorting order");
+            }
+        }
+
+        private static void SortAscending<T>(ref T[] a) where T : IComparable
+        {
             for (int outC = 0; outC < a.Length - 1; outC++)
             {
                 for (int inC = 0; inC < a.Length - 1 - outC; inC++)
                 {
-                    switch (order)
+                    if (Methods.IsFirstBigger(a[inC], a[inC + 1]))
                     {
-                        case SortingOrder.Ascending:
-                            if (Methods.IsFirstBigger(a[inC], a[inC + 1]))
-                            {
-                                Sorting.Methods.Swap(ref a[inC], ref a[inC + 1]);
-                            }
-                            break;
-                        case SortingOrder.Descending:
-                            if (!Methods.IsFirstBigger(a[inC], a[inC + 1]))
-                            {
-                                Sorting.Methods.Swap(ref a[inC], ref a[inC + 1]);
-                            }
-                            break;
-                        default:
-                            break;
+                        Sorting.Methods.Swap(ref a[inC], ref a[inC + 1]);
+                    }
+                }
+            }
+        }
+
+        private static void SortDescending<T>(ref T[] a) where T : IComparable
+        {
+            for (int outC = 0; outC < a.Length - 1; outC++)
+            {
+                for (int inC = 0; inC < a.Length - 1 - outC; inC++)
+                {
+                    if (!Methods.IsFirstBigger(a[inC], a[inC + 1]))
+                    {
+                        Sorting.Methods.Swap(ref a[inC], ref a[inC + 1]);
                     }
                 }
             }
