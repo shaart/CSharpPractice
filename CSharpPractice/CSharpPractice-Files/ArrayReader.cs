023a854ff122bf0411e902150dd931ca82c22327
Artur <shalaev-artur@yandex.ru>
2017-09-29 02:16:45 +0300

    'Files' moved to external lib

---
+++ CSharpPractice/CSharpPractice-Files/ArrayReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleToAttribute("CSharpPractice.UnitTest")]
+
+namespace CSharpPractice.Files
+{
+    public static class ArrayReader
+    {
+        public static int[] ReadFromFileTxt(string filename)
+        {
+            List<int> resultList = new List<int>();
+            try
+            {
+                using (StreamReader file = new StreamReader(filename))
+                {
+                    while (!file.EndOfStream)
+                    {
+                        resultList.Add(file.Read());
+                    }
+                }
+                return resultList.ToArray<int>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
