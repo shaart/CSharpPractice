4e5664534b81a96d44c04ded9e1f5f5631178032
Artur <shalaev-artur@yandex.ru>
2017-09-29 06:46:33 +0300

    FilesPlugin initial commit

---
+++ CSharpPractice/CSharpPractice-Files/FilesPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using PluginContracts;
+
+namespace CSharpPractice.Encryption
+{
+    public class FilesPlugin : IPlugin
+    {
+        public string Name { get { return "Files"; } }
+        public string Author { get { return "Shalaev Artur"; } }
+        public string Description { get { return "Working with files"; } }
+        
+        public HashSet<ICommand> Commands
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
